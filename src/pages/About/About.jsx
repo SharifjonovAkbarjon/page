9e6d7c65f5c24d87c8d667b5638082b966e81781
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-08-13 23:24:06 +0500

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <>
-        <h2 className='text-gray-700'>Katalog</h2>
+        <h2 className='text-gray-700'>Katalog
+          <p>mebel</p>
+          fwounkjcdm,s
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas corrupti architecto vel dignissimos non mollitia odio quos eius voluptates fuga modi nisi repudiandae sint assumenda incidunt quae, obcaecati eveniet exercitationem! Quibusdam vel reprehenderit ab placeat id, praesentium pariatur dolorum labore veritatis error iste, similique sint reiciendis tempore distinctio doloremque nisi?</p>
+        </h2>
     </>
   )
 }
