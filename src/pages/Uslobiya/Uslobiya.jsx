076f7ef52f123ac72b42c120a07a3a8038a66a74
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-08-14 10:45:12 +0500

    notFound

--- src/pages/Uslobiya/Uslobiya.jsx
+++ src/pages/Uslobiya/Uslobiya.jsx
@@ -6,7 +6,7 @@ const Uslobiya = () => {
     const Navigate = useNavigate()
   return (
     <div className='container mx-auto  text-center'>
-        <img className='inline-block relative w-full mt-[40px] m-auto' src={not} alt="" />
+        <img className='inline-block relative w-full mt-[40px] m-auto mb-[-150px]'  src={not} alt="" />
         <button onClick={()=>Navigate("/")} className='px-[120px] absolute top-[834px] left-[735px] text-white font-bold py-[28px] bg-[rgb(0,120,219)] mt-[-00px] rounded-[40px]'>BACK TO HOME</button>
     </div>
   )
